3739e7c2901afd161a1da7842c2f26a4c0a775c3
Piero Viano <piero.viano@gmail.com>
2022-11-19 04:36:59 +0200

    ''

--- Build.Debug.bat
+++ Build.Debug.bat
@@ -1,10 +1,14 @@
-mklink /j Packages ..\WhenTheVersion\Packages
-del ..\WhenTheVersion\Packages\PdfRepresantation.*
+if exist ..\WhenTheVersion\Packages\ (
+  mklink /j Packages ..\WhenTheVersion\Packages 
+) else (
+  md Packages
+)
+del Packages\PdfRepresantation.*
 rmdir /s /q %userprofile%\.nuget\Packages\PdfRepresantation
 nuget restore PdfRepresantation.sln
 MSBuild.exe PdfRepresantation.sln /property:Configuration=Debug 
-copy "PdfRepresantation\bin\Debug\PdfRepresantation.1.0.0.nupkg" ..\WhenTheVersion\Packages\
-copy "PdfRepresantation\bin\Debug\PdfRepresantation.1.0.0.symbols.nupkg" ..\WhenTheVersion\Packages\
+copy "PdfRepresantation\bin\Debug\PdfRepresantation.1.0.0.nupkg" Packages\
+copy "PdfRepresantation\bin\Debug\PdfRepresantation.1.0.0.symbols.nupkg" Packages\
 git add -A
 git commit -a --allow-empty-message -m ''
 git push
