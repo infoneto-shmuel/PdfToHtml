b00c9c8802aac4ed3ce1660598d5d3f2b8a12f02
Piero Viano <piero.viano@gmail.com>
2022-11-19 04:30:26 +0200

    Cleaned code

--- PdfRepresantation/Extensions/PdfWriterExtension.cs
+++ PdfRepresantation/Extensions/PdfWriterExtension.cs
@@ -14,6 +14,10 @@ namespace PdfRepresantation.Extensions
 {
     public static class PdfWriterExtension
     {
+        private const string Html = ".html";
+        private const string Json = ".json";
+        private const string Xml = ".xml";
+
         public static List<string> ConvertToHtml(this FileInfo file, string targetDirectory)
         {
             List<string> paths = null;
@@ -22,7 +26,7 @@ namespace PdfRepresantation.Extensions
 
         public static List<string> ConvertToHtml(this FileInfo file, string targetDirectory, ref List<string> paths)
         {
-            var details = GetPdfDetails(file, ".html", targetDirectory, ref paths, out var target);
+            var details = GetPdfDetails(file, Html, targetDirectory, ref paths, out var target);
             var writer = new PdfHtmlWriter(new HtmlWriterConfig { UseCanvas = false });
             writer.SaveAs(details, target, false);
             return paths;
@@ -37,7 +41,7 @@ namespace PdfRepresantation.Extensions
         public static List<string> ConvertToHtmlTables(this FileInfo file, string targetDirectory,
             ref List<string> paths)
         {
-            var details = file.GetPdfDetails(".html", targetDirectory, ref paths, out var target);
+            var details = file.GetPdfDetails(Html, targetDirectory, ref paths, out var target);
             var writer = new PdfXmlWriter();
             var tableModels = writer.ToTables(details);
 
@@ -79,7 +83,7 @@ namespace PdfRepresantation.Extensions
 
         public static List<string> ConvertToJson(this FileInfo file, string targetDirectory, ref List<string> paths)
         {
-            var details = file.GetPdfDetails(".json", targetDirectory, ref paths, out var target);
+            var details = file.GetPdfDetails(Json, targetDirectory, ref paths, out var target);
             var writer = new PdfJsonWriter();
             writer.SaveAs(details, target, true);
             return paths;
@@ -95,7 +99,7 @@ namespace PdfRepresantation.Extensions
             ref List<string> paths)
         {
             var writer = new PdfJsonWriter();
-            var details = file.GetPdfDetails(".json", targetDirectory, ref paths, out var target);
+            var details = file.GetPdfDetails(Json, targetDirectory, ref paths, out var target);
             writer.SaveAsJsonTables(details, target);
             return paths;
         }
@@ -108,7 +112,7 @@ namespace PdfRepresantation.Extensions
 
         public static List<string> ConvertToXml(this FileInfo file, string targetDirectory, ref List<string> paths)
         {
-            var details = file.GetPdfDetails(".xml", targetDirectory, ref paths, out var target);
+            var details = file.GetPdfDetails(Xml, targetDirectory, ref paths, out var target);
             var writer = new PdfXmlWriter();
             writer.SaveAs(details, target);
             return paths;
@@ -123,7 +127,7 @@ namespace PdfRepresantation.Extensions
         public static List<string> ConvertToXmlTables(this FileInfo file, string targetDirectory,
             ref List<string> paths)
         {
-            var details = file.GetPdfDetails(".xml", targetDirectory, ref paths, out var target);
+            var details = file.GetPdfDetails(Xml, targetDirectory, ref paths, out var target);
             var writer = new PdfXmlWriter();
             writer.SaveAsXmlTables(details, target);
             return paths;
@@ -132,6 +136,7 @@ namespace PdfRepresantation.Extensions
         public static PdfDetails GetPdfDetails(this FileInfo file, string extension,
             string targetDirectory, ref List<string> paths, out string target)
         {
+            Directory.CreateDirectory(targetDirectory);
             paths ??= new List<string>();
             var name = Path.GetFileNameWithoutExtension(file.Name);
             var details = PdfDetailsFactory.Create(file.FullName);
