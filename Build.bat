0818fb3486a8249ccfa87c11f83409612e2afcd0
Piero Viano <piero.viano@gmail.com>
2023-05-18 02:49:47 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 IF NOT DEFINED Configuration SET Configuration=Release
+MSBuild.exe PdfRepresentation.sln -t:clean
 MSBuild.exe PdfRepresentation.sln -t:restore -p:RestorePackagesConfig=true
 MSBuild.exe PdfRepresentation.sln -m /property:Configuration=Debug
 git push
